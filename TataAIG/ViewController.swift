f474d7337eca089783a2797637450ca8973ed136
Chaitanya Deshpande <1679159@tcs.com>
2021-06-22 14:37:32 +0530

    Initial Commit

---
+++ TataAIG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TataAIG
+//
+//  Created by Chaitanya Deshpande on 22/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

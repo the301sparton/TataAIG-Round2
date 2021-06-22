f26d120f2ea044eb44c28eb6994786f354e51a87
Chaitanya Deshpande <1679159@tcs.com>
2021-06-22 15:30:16 +0530

    Create Project, Add JSON Models.

---
+++ TataAIG/Model/Vehicle.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  TataAIG
+//
+//  Created by Chaitanya Deshpande on 22/06/21.
+
+import Foundation
+struct Vehicle : Codable {
+	let id : Int?
+	let coordinate : VehicleCoordinate?
+	let fleetType : String?
+	let heading : Double?
+
+	enum CodingKeys: String, CodingKey {
+
+		case id = "id"
+		case coordinate = "coordinate"
+		case fleetType = "fleetType"
+		case heading = "heading"
+	}
+
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		id = try values.decodeIfPresent(Int.self, forKey: .id)
+		coordinate = try values.decodeIfPresent(VehicleCoordinate.self, forKey: .coordinate)
+		fleetType = try values.decodeIfPresent(String.self, forKey: .fleetType)
+		heading = try values.decodeIfPresent(Double.self, forKey: .heading)
+	}
+
+}
---
+++ TataAIG/Model/VehicleArray.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TataAIG
+//
+//  Created by Chaitanya Deshpande on 22/06/21.
+
+import Foundation
+struct VehicleArray : Codable {
+	let poiList : [Vehicle]?
+
+	enum CodingKeys: String, CodingKey {
+		case poiList = "poiList"
+	}
+
+	init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		poiList = try values.decodeIfPresent([Vehicle].self, forKey: .poiList)
+	}
+
+}
